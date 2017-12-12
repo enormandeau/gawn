e2e1cdfac25ee223def00e6477354e7ff22bd656
Eric Normandeau <eric.normandeau.qc@gmail.com>
2017-12-12 10:24:38 -0500

    Change default config params

--- 02_infos/gawn_config.sh
+++ 02_infos/gawn_config.sh
@@ -8,12 +8,12 @@
 NCPUS=10                    # Number of CPUs to use for analyses (int, 1+)
 
 # Genome indexing
-SKIP_GENOME_INDEXING=1      # 1 to skip genome indexing, 0 to index it
+SKIP_GENOME_INDEXING=0      # 1 to skip genome indexing, 0 to index it
 
 # Genome annotation with transcriptome
 # NOTE: do not use compressed fasta files
-GENOME_NAME="test_genome.fasta"                  # Name of genome fasta file found in 03_data
-TRANSCRIPTOME_NAME="test_transcriptome.fasta"    # Name of transcriptome fasta file found in 03_data
+GENOME_NAME="genome.fasta"                  # Name of genome fasta file found in 03_data
+TRANSCRIPTOME_NAME="transcriptome.fasta"    # Name of transcriptome fasta file found in 03_data
 
 # Swissprot
 SWISSPROT_DB="~/Software/blastplus_databases/swissprot"
